75e63aeda5196814d7f706a6c1a2d2faac2d69d8
piyush9012 <121113213+piyush9012@users.noreply.github.com>
2023-05-09 10:33:13 +0530

    Add files via upload

---
+++ program2.java
@@ -0,0 +1,25 @@
+// Design a java code to accept a value for n and calculate the total number of all possible squares in a square matrix
+import java.util.*;
+public class program2
+{
+    public static void main(String[] args)
+    {
+        int size, sum=0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the matrix");
+        size = sc.nextInt();
+        if (size<1)
+        {
+            System.out.println("Invalid Input");
+        }    
+        else
+        {
+            while(size!=0)
+            {
+                sum += size*size;
+                size--;
+            }
+        }
+        System.out.println("Total number of square matrices are "+ sum);
+    }
+}
