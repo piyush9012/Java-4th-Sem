afbd49807df597a4ba4c82545d216af724be8d14
piyush9012 <121113213+piyush9012@users.noreply.github.com>
2023-06-06 09:42:08 +0530

    Password Generator

---
+++ Password_generator.java
@@ -0,0 +1,47 @@
+import java.util.*;
+public class password_generator
+{
+    public static void main(String args[])
+    {
+        int rno, sum = 0, rem;
+        long ph;
+        String pw = new String();
+        String name;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Room Number");
+        rno = sc.nextInt();
+        int rno1 = rno;
+        System.out.println("Enter the Mobile Number");
+        ph = sc.nextLong();
+        System.out.println("Enter the Name");
+        name = sc.next();
+        char []sym = { ')','@','#','$','%','^','!','&','(','*','~'};
+        do
+        {
+            while(rno != 0)
+            {
+                rem = rno%10;
+                sum = sum +rem;
+                rno = rno/10;
+            }
+            if (sum>= 10)
+            {
+                rno = sum;
+                sum = 0;   
+            }
+        }while(rno!= 0);
+        int c = rno1/100;
+        int len = name.length();
+        int pos = 0;
+        for (int i = 0; i < len; i++)
+        {
+            if(c == i)
+            {
+                pos = i;
+                System.out.println("Password is: ");
+            }    
+        }
+        pw = ""+name.charAt(0)+(ph%10)+sum+sym[c]+name.charAt(len-1);
+        System.out.println(pw);
+    }
+}
