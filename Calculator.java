5df8ef951f0e336ff2ec034aad44ae854c2f6ff3
piyush9012 <121113213+piyush9012@users.noreply.github.com>
2023-06-19 00:46:03 +0530

    Simple Calculator

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,6 @@
 import java.util.*;
-public class Calculator
-{
-    public static void main(String args[])
-    {
+public class simple_calculator {
+    public static void main(String args[]) {
         int op1, op2, res;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter ther first operand");
@@ -10,25 +8,29 @@ public class Calculator
         System.out.println("Enter the second operand");
         op2 = sc.nextInt();
         char operator;
-        System.out.println("Enter the operator: ");
+        System.out.print("Enter the operator (+,-,*,/,%): ");
+        System.out.println();
         operator = sc.next().charAt(0);
-        switch (operator)
-        {
+        switch (operator) {
             case '+':
-                res = op1+op2;
-                System.out.println("The addition is "+ res);
+                res = op1 + op2;
+                System.out.println("The addition is " + res);
                 break;
             case '-':
-                res = op1-op2;
-                System.out.println("The subtraction is "+ res);
+                res = op1 - op2;
+                System.out.println("The subtraction is " + res);
                 break;
             case '*':
-                res = op1*op2;
-                System.out.println("The multiplication is "+ res);
-                    break;
+                res = op1 * op2;
+                System.out.println("The multiplication is " + res);
+                break;
             case '/':
-                res = op1/op2;
-                System.out.println("The division is "+ res);
+                res = op1 / op2;
+                System.out.println("The division is " + res);
+                break;
+            case '%':
+                res = op1 % op2;
+                System.out.println("The division is " + res);
                 break;
             default:
                 System.out.println("Invalid Input");
@@ -36,9 +38,3 @@ public class Calculator
         }
     }
 }
-
-// Write a java program with one method to put even and odd elements of an array in two separate arrays and another method to find the transpose of the matrix by implementing the concept of method overwriting
-
-// super keyword in java is a reference variable which is used to refer to immediate parent class wheever you create the instance of subclaass an instance of parent class is created implicitly which is referred by super reference variable
-
-// extends keyword
