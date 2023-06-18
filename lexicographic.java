e23bd3fac30df467b90b891b99ccab34c086812c
piyush9012 <121113213+piyush9012@users.noreply.github.com>
2023-06-18 23:47:38 +0530

    Lexicographic Order

--- lexicographic.java
+++ lexicographic.java
@@ -1,20 +1,16 @@
 import java.util.*;
-public class lexicographic
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Input the names ");
-        String[] array = new String[5];            // Declaration of string array      
-        Scanner sc = new Scanner(System.in);  
-        for(int i=0; i<5; i++)
-        {
+public class lexicographic {
+    public static void main(String[] args) {
+        System.out.println("Enter five names: ");
+        String array[] = new String[5];       // Declaration of string array
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
             array[i] = sc.nextLine();
         }
         Arrays.sort(array);
         System.out.print("Sorted list is: ");
-        for(int i=0; i<5; i++)
-        {
-            System.out.print(array[i]+" ");
+        for (int i = 0; i < 5; i++) {
+            System.out.print(array[i] + " ");
         }
     }
 }
