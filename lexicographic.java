fa06013fe62c0d2395d447128ddf15b2f7f62b5b
piyush9012 <121113213+piyush9012@users.noreply.github.com>
2023-06-18 23:46:46 +0530

    Lexicographic Order

--- lexicographic.java
+++ lexicographic.java
@@ -1,12 +1,10 @@
-// Design a java code to accept 5 strings from the user and print them in lexico graphical order
-
 import java.util.*;
-public class lemicographic
+public class lexicographic
 {
     public static void main(String[] args)
     {
         System.out.println("Input the names ");
-        String[] array = new String[5];             // Declaration of string array      
+        String[] array = new String[5];            // Declaration of string array      
         Scanner sc = new Scanner(System.in);  
         for(int i=0; i<5; i++)
         {
