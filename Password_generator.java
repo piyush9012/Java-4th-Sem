ad7cb1b2a422eb76d2646be522b4767b5f5bc21c
Piyush Narayan <121113213+piyush9012@users.noreply.github.com>
2023-07-13 19:46:28 +0530

    Password Generator

--- Password_generator.java
+++ Password_generator.java
@@ -1,9 +1,7 @@
 import java.util.*;
-public class password_generator
-{
-    public static void main(String args[])
-    {
-        int rno, sum = 0, rem;
+public class password_generator {
+    public static void main(String args[]) {
+        int rno, sum = 0, temp;
         long ph;
         String pw = new String();
         String name;
@@ -15,25 +13,22 @@ public class password_generator
         ph = sc.nextLong();
         System.out.println("Enter the Name");
         name = sc.next();
-        char []sym = { ')','@','#','$','%','^','!','&','(','*'};
-        do
-        {
-            while(rno != 0)
-            {
-                rem = rno%10;
-                sum = sum +rem;
-                rno = rno/10;
+        char sym[] = { ')', '@', '#', '$', '%', '^', '!', '&', '(', '*' };
+        while(rno != 0) {
+            while (rno != 0) {
+                temp = rno % 10;
+                sum = sum + temp;
+                rno = rno / 10;
             }
-            if (sum>= 10)
-            {
+            if (sum >= 10) {
                 rno = sum;
-                sum = 0;   
+                sum = 0;
             }
-        }while(rno!= 0);
-        int c = rno1/100;
+        }
+        int c = rno1 / 100;
         int len = name.length();
         System.out.println("Password is: ");
-        pw = ""+name.charAt(0)+(ph%10)+sum+sym[c]+name.charAt(len-1);
+        pw = "" + name.charAt(0) + (ph % 10) + sum + sym[c] + name.charAt(len - 1);
         System.out.println(pw);
     }
 }
