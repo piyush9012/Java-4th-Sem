f4336353aeb0f4d8fa5672add6c41b5d34db1f9d
Piyush Narayan <121113213+piyush9012@users.noreply.github.com>
2023-07-23 01:44:01 +0530

    Employee Salary

--- employee.java
+++ employee.java
@@ -1,10 +1,8 @@
-import java.util.Date;
-import java.util.Stack;
 import java.util.*;
 
-public class employee{
-	public static void main(String[] args) {
-		int[] EmpId = { 1001, 1002, 1003, 1004, 1005, 1006, 1007 };
+public class prac{
+    public void calc(int id){
+        int[] EmpId = { 1001, 1002, 1003, 1004, 1005, 1006, 1007 };
 		String[] EmpName = { "Abc", "Opqr", "Ghi", "Wxyz", "Jklmn", "Stuv", "Def" };
 		String[] JoinDate = { "01/04/2009", "23/08/2012", "12/11/2008", "29/01/2013", "16/07/2005", "01/01/2000","12/06/2006" };
 		char[] DesigCode = { 'e', 'c', 'k', 'r', 'm', 'e', 'c' };
@@ -15,28 +13,37 @@ public class employee{
 		char[] DesignationCode = { 'e', 'c', 'k', 'r', 'm' };
 		String[] Designation = { "Engineer", "Consultant", "Clerk", "Receptionist", "Manager" };
 		double[] DA = { 20000, 32000, 12000, 15000, 40000 };
-		int flag = 0;
-		int n;
-		Scanner sc =  new Scanner(System.in);
-		System.out.println("Enter Valid Employee ID: ");
-		n = sc.nextInt();
-		int id = n;
-		for (int i = 0; i < EmpId.length; i++) {
+        for (int i = 0; i < EmpId.length; i++) {
 			if (EmpId[i] == id) {
-				flag = 1;
-				System.out.println("Emp Id.	 Emp Name	Department	Designation	   DA");
-				System.out.print(EmpId[i] + " 	 " + EmpName[i] + " 		" + Department[i]);
+				System.out.println("Emp Id\tEmp Name  Department\tDesignation\tDA\tSalary");
+				System.out.print(EmpId[i]+"\t"+EmpName[i]+"\t\t"+Department[i]);
 				for (int j = 0; j < DesignationCode.length; j++) {
 					if (DesigCode[i] == DesignationCode[j]) {
-						System.out.print(" 		" + Designation[j] + " 	");
-						double sum = Basic[i] + HRA[i] + DA[j] - IT[i];
-						System.out.print(sum);
-						System.out.println();
+						System.out.print("\t"+Designation[j]+"\t"+DA[i]+"\t");
+						double sum = Basic[i]+HRA[i]+DA[j]-IT[i];
+						System.out.println(sum);
 					}
 				}
 			}
 		}
-		if (flag == 0)
-			System.out.println("There is no employee with EmpId : " + id);
+    }
+
+	public static void main(String[] args) {
+        prac obj = new prac();
+		Scanner sc =  new Scanner(System.in);
+		System.out.println("Enter Employee ID: ");
+		int id = sc.nextInt();
+        switch (id) {
+            case 1001: obj.calc(id); break;
+            case 1002: obj.calc(id); break;
+            case 1003: obj.calc(id); break;
+            case 1004: obj.calc(id); break;
+            case 1005: obj.calc(id); break;
+            case 1006: obj.calc(id); break;
+            case 1007: obj.calc(id); break;
+            default:
+                System.out.println("There is no employee with EmpId: " + id);
+                break;
+        }
 	}
 }
