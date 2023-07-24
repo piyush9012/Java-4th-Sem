848ac72df7cb1c2dd8164cb31f75070182fb282f
Piyush Narayan <121113213+piyush9012@users.noreply.github.com>
2023-07-24 21:48:17 +0530

    Exception Handling

--- exception_handling.java
+++ exception_handling.java
@@ -1,16 +1,23 @@
-import java.util.*;
 public class exception_handling {
     public static void main(String[] args) {
         try {
-            try {
-                int[] a = { 1, 2, 3 };
-                System.out.println(a[3]);
-            } catch (ArrayIndexOutOfBoundsException e) {
-                System.out.println("Exception Type: "+ e);
-            }
-            System.out.println(4 / 0);
-        } catch (ArithmeticException e) {
+            int num = 4;
+            System.out.println(num/0);
+        }catch (ArithmeticException e) {
             System.out.println("Exception Type: "+ e);
+        }
+        try {
+            int a[] = { 1, 2, 3 };
+            System.out.println(a[3]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Exception Type: "+ e);
+            }
+        try{
+            String str = null;
+            int len = str.length(); // This line will throw a NullPointerException
+            System.out.println("Length of the string: " + len);  
+        } catch(NullPointerException e) {
+            System.out.println("Exception Type: " + e);
         }
     }
 }
