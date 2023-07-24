d56ef1556edbde39b1589f2f9e997150e4f96de0
Piyush Narayan <121113213+piyush9012@users.noreply.github.com>
2023-07-24 21:29:34 +0530

    Doubly MinMax

--- MinMax.java
+++ MinMax.java
@@ -7,50 +7,53 @@ public class MinMax {
             this.data = data;
         }
     }
+    // Represent the head and tail of the doubly linked list  
+    Node head = null;
 
-    Node head, tail = null;
-
-    public void addNode(int data) { // addNode() will add a node to the list
-        Node newNode = new Node(data);
-        if (head == null) {
-            head = tail = newNode;
-            head.prev = tail.next = null;
-        } else {
-            tail.next = newNode;
-            newNode.prev = tail;
-            tail = newNode;
-            tail.next = null;
+    public void insert_node(int data) {    //  addNode() will add a node to the list
+        Node newNode = new Node(data);     // Create a new node  
+        if (head == null) {                // If list is empty  
+            head = newNode;         //Both head and tail will point to newNode  
+            head.prev = head.next = null;  //head's & tail's previous will point to null  
+        }
+        else {  
+            head.next = newNode;         //newNode will be added after tail such that tail's next will point to newNode
+            newNode.prev = head;         //newNode's previous will point to tail   
+            head = newNode;              //newNode will become new tail
+            head.next = null;            //As it is last node, tail's next will point to null  
         }
     }
 
-    public int minNode() { // Find the minimum node
-        Node temp = head;
+    public int minNode() {              // Find the minimum node
+        Node temp = head;               // Node temp will point to head  
         int min;
-        if (head == null) {
+        if (head == null) {             //Checks if list is empty  
             System.out.println("List is empty");
             return 0;
-        } else {
-            min = head.data;
+        }
+        else {
+            min = head.data;                //Initially, min will store the value of head's data  
             while (temp != null) {
-                if (min > temp.data)
-                    min = temp.data;
+                if (min > temp.data)        // If value of min greater than temp data
+                    min = temp.data;        // then replace value of min with temp node's data  
                 temp = temp.next;
             }
         }
         return min;
     }
 
-    public int maxNode() { // Find the maximum node
-        Node temp = head;
+    public int maxNode() {           // Find the maximum node
+        Node temp = head;            // Node temp will point to head 
         int max;
-        if (head == null) {
+        if (head == null) {         //Checks if list is empty  
             System.out.println("List is empty");
             return 0;
-        } else {
-            max = head.data;
+        }
+        else {
+            max = head.data;             //Initially, max will store the value of head's data
             while (temp != null) {
-                if (temp.data > max)
-                    max = temp.data;
+                if (max < temp.data)     //If value of max is lesser than temp data  
+                    max = temp.data;     //Then replace value of max with temp node's data
                 temp = temp.next;
             }
         }
@@ -59,11 +62,11 @@ public class MinMax {
 
     public static void main(String[] args) {
         MinMax dList = new MinMax();
-        dList.addNode(5);
-        dList.addNode(7);
-        dList.addNode(9);
-        dList.addNode(1);
-        dList.addNode(2);
+        dList.insert_node(5);      //Add nodes to the list  
+        dList.insert_node(7);
+        dList.insert_node(9);
+        dList.insert_node(1);
+        dList.insert_node(2);
         System.out.println("Minimum value node in the list: " + dList.minNode());
         System.out.println("Maximum value node in the list: " + dList.maxNode());
     }
